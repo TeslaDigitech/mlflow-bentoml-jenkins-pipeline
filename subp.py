556a48463cb1d682eebbacfc57817a2004fcc581
Pranav Sadagopan <86850478+pranav1699@users.noreply.github.com>
2023-02-14 23:08:05 +0530

    Update subp.py

--- subp.py
+++ subp.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 
-process = subprocess.run(['bentoml'],check=True,stdout=subprocess.PIPE,universal_newlines=True)
+process = subprocess.run(['bentoml --version'],check=True,stdout=subprocess.PIPE,universal_newlines=True)
 
 output = process.stdout
 
