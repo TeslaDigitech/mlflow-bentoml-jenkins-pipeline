f32a9667db81333b57a517e568fd91145d71ebcf
Pranav Sadagopan <86850478+pranav1699@users.noreply.github.com>
2023-02-15 15:56:20 +0530

    Update chosing_model.py

--- chosing_model.py
+++ chosing_model.py
@@ -19,9 +19,6 @@ print("Printing model uri")
 # print(artifact_path)
 print("Printing model")
 
-if bentoml.models.get(f"diabetes_pred_elastic:{run_id}"):
-        print("Model already exist")
-else:
-        print("Saving the model")
-        bentoml.sklearn.save_model(f"diabetes_pred_elastic:{run_id}", model)
+print("Saving the model")
+bentoml.sklearn.save_model(f"diabetes_pred_elastic:{run_id}", model)
         
